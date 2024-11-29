cb0823b044843f2b535c2fa1ba55c3efe8d20577
Muhammad Syauqi Murtadlo <sauki084@gmail.com>
2024-11-29 20:18:46 +0700

    Menambahkan file PHP

---
+++ php-codepolitan/index.php
@@ -0,0 +1,65 @@
+<?php
+
+// echo 20 * 8;
+// "<br>";
+// echo "Ini adalah bahasa pemrogramman PHP";
+// ini adalah komentar di bahasa pemrogramman PHP
+// titik kome pada bahasa pemrogramman PHP ini digunakan untuk mengakhiri sebuah baris perintah bahwa baris perintah tersebut sudah selesai
+
+// $namaLengkap = "Muhammad Syauqi Murtadlo";
+// echo $namaLengkap;
+
+// $y = 20;
+// $x = 30;
+// $totalKeseluruhan = $y - $x;
+// print_r($totalKeseluruhan);
+
+// $namaDepan = "Muhammad";
+// $namaTengah = "Syauqi";
+// $namaBelakang = "Murtadlo";
+// $nama = "Nama Saya $namaDepan $namaTengah $namaBelakang";
+// echo $nama;
+
+// ? TERUS BELAJAR.......
+
+
+// $nama = "Muhammad Syauqi Murtadlo";
+// $ttl = "20 Februari 2004";
+// $alamat = "Gedang Tambakrejo Jombang";
+// 
+
+$pemainBola = strtoupper("erling haaland");
+// echo $pemainBola;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h2>Selamat Datang<?= $pemainBola ?> di Manchester City</h2>
+</body>
+
+</html>
+
+<!-- // <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+// </head>
+
+// <body>
+//   <h1>Selamat Datang <?= $nama ?></h1>
+//   <h2>Tempat tanggal lahir : <?= $ttl ?></h2>
+//   <h2>Alamat : <?= $alamat ?></h2>
+// </body>
+
+// </html> -->
